f0450015e948f47ee1acdf951e31fcde83138f3f
eternityfantastic <1544152874@qq.com>
2019-09-23 22:43:36 +0800

    add

--- Java代码/Homework/src/Day07.java
+++ Java代码/Homework/src/Day07.java
@@ -26,17 +26,23 @@ class Calculator{
         return num1;
     }
 
-    public void setNum1(int num1) {
-        this.num1 = num1;
-    }
-
     public int getNum2() {
         return num2;
     }
-
-    public void setNum2(int num2) {
-        this.num2 = num2;
+    public int add(){
+        return this.num1 + this.num2;
     }
+    public int sub(){
+        return this.num1 - this.num2;
+    }
+    public int multiply(){
+        return this.num1 * this.num2;
+    }
+    public int div(){
+        return this.num1 / this.num2;
+    }
+
+
     public void swap(){
         int temp;
         temp = this.num2;
