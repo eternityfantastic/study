3007c1f87071935fded1704b1efb04d941d69797
eternityfantastic <1544152874@qq.com>
2019-10-15 22:48:55 +0800

    add

--- Java代码/Homework/src/Test.java
+++ Java代码/Homework/src/Test.java
@@ -1,16 +1,36 @@
 public class Test {
+    static int j = 0;
+
     public static void main(String[] args) {
-//        String s1 = "a";
-//        String s = "ab";
-//        String s2 = s1+"b";
-//        String s3 = "a"+"b";
-//        System.out.println(s2 == "ab");
-//        System.out.println(s3 == s);
-//        int a = Integer.MAX_VALUE;
-//        short b = Short.MIN_VALUE;
-//        System.out.println(a+1);
-//        System.out.println(b);
-        char a = 1;
-        char b = a + 1;
+        //static int j = 0;
+//        for (int i = 0; i < 100; i++) {
+//            j = j++;
+//        }
+//        System.out.println(j);
+//        short s1 = 0;
+//        s1 += 1;
+//        System.out.println(s1);
+//        System.out.println("value is " + ((s1 < 0) ? 10.8 : 8));
+//        char x = 'x';
+//        int i = 10;
+//        System.out.println(false ? i : x);
+//        System.out.println(false ? 10 : x);
+//        int num = 3;
+//        System.out.println(num>>>1);
+//        int i = 1;
+//        int sum = 0;
+//        int temp = 1;
+//        while(i <= 5){
+//            temp*=i;
+//            sum+=temp;
+//            i++;
+//        }
+//        System.out.println(sum);
+//    }
+    long a =14;
+    System.out.format("%x\n",a);
+    System.out.format("%d%n",a);
+    System.out.printf("%d\n",a);
+    System.out.printf("%o",a);
     }
 }
