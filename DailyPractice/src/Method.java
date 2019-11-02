c0aad6a1200a7fb23e144f12598c5dad785f6ad1
eternityfantastic <1544152874@qq.com>
2019-11-02 21:11:00 +0800

    add

---
+++ DailyPractice/src/Method.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public abstract class Method {
+    int a;
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    public Method(int ad) {
+        a = ad;
+    }
+
+
+}
