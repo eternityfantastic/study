0ab8d5951d353237a4436416c752257fda3148c4
eternityfantastic <1544152874@qq.com>
2019-11-04 23:01:00 +0800

    add

---
+++ DailyPractice/src/StringConstruct.java
@@ -0,0 +1,29 @@
+public class StringConstruct {
+    public static void main(String[] args) {
+//        String s1=new String("A")+new String("B");
+//        System.out.println(s1==s1.intern());
+//        System.out.println(s1=="AB");
+        String S1 = "A";
+        String S2 = "B";
+        String s1=S1+S2;
+        //
+        //System.out.println(s1==s1.intern());
+        System.out.println(s1=="AB");
+
+//        String s = new String("A");
+//        s.intern();
+//        String s2 = "A";
+//        System.out.println(s == s2);
+
+
+//        String s3 = new String("1") + new String("1");
+//        s3.intern();
+//        String s4 = "11";
+//        System.out.println(s3 == s4);
+//
+//        byte[] arr = {98,99,100};
+//        String str = new String(arr);
+//        String s = "ABCD";
+//        String s2 = new String("A");
+    }
+}
