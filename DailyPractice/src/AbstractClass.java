aca5c5dd866260a07407a72a48306c97d3c18746
eternityfantastic <1544152874@qq.com>
2019-11-08 21:08:40 +0800

    add

---
+++ DailyPractice/src/AbstractClass.java
@@ -0,0 +1,5 @@
+public abstract class AbstractClass {
+    public static void method(){
+        System.out.println("可以使用静态方法");
+    }
+}
--- DailyPractice/src/StringConstruct.java
+++ DailyPractice/src/StringConstruct.java
@@ -1,21 +1,23 @@
+import java.io.UnsupportedEncodingException;
+
 public class StringConstruct {
-    public static void main(String[] args) {
+    public static  final synchronized void main(String[] args) throws UnsupportedEncodingException {
 //        String s1=new String("A")+new String("B");
 //        System.out.println(s1==s1.intern());
 //        System.out.println(s1=="AB");
-        String S1 = "A";
-        String S2 = "B";
-        String s1=S1+S2;
-        //
-        //System.out.println(s1==s1.intern());
-        System.out.println(s1=="AB");
-
+//        String S1 = "A";
+//        String S2 = "B";
+//        String s1=S1+S2;
+//
+//        System.out.println(s1==s1.intern());
+//        System.out.println(s1=="AB");
+//
 //        String s = new String("A");
 //        s.intern();
 //        String s2 = "A";
 //        System.out.println(s == s2);
-
-
+//
+//
 //        String s3 = new String("1") + new String("1");
 //        s3.intern();
 //        String s4 = "11";
@@ -25,5 +27,12 @@ public class StringConstruct {
 //        String str = new String(arr);
 //        String s = "ABCD";
 //        String s2 = new String("A");
+
+
+        /**
+         * String​(byte[] bytes)构造方法演示；
+         */
+        String s = new String(new byte[]{98,99},"utf8");
+        System.out.println(s);
     }
 }
