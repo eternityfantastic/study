531b5163f764e9f8be6753f2afd880a95bf29f8b
eternityfantastic <1544152874@qq.com>
2019-11-09 20:33:41 +0800

    add

--- DailyPractice/src/Test.java
+++ DailyPractice/src/Test.java
@@ -1,19 +1,15 @@
-import java.util.Arrays;
-
 public class Test {
     public static void main(String[] args) {
-        AbstractClass.method();
-//        int[] nums = {1,4,3,2,5,6};
-//        Arrays.sort(nums);
-//        System.out.println(Arrays.toString(nums));
-//        int sum = 0;
-//        for(int i = 1 ; i < nums.length ; i += 2){
-//            sum += (nums[i] - nums[i - 1]);
-//        }
-//        System.out.println(sum);
-//        Solution s = new Solution();
-//        System.out.println( s.arrayPairSum(nums));
+        A a = new B();
     }
+}
+abstract class A{
+    protected abstract float f();
+}
+class B extends A{
 
-
+    @Override
+    public float f(){
+        return  1;
+    }
 }
