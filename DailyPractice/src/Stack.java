01db225ae1f6cbfcf843b929b9cb47dd32b3ae78
eternityfantastic <1544152874@qq.com>
2019-11-15 16:46:34 +0800

    add

---
+++ DailyPractice/src/Stack.java
@@ -0,0 +1,20 @@
+public class Stack<T> {
+    int top ;
+    int size;
+    T [] myStack = (T[]) new Object[100];
+    public T push( T num){
+        myStack[top++] = num;
+//        return top[];
+        return myStack[top-1];
+    }
+    //出栈
+    public  T pop(){
+        //判断栈空否？
+        return myStack[--top];
+    }
+    //得到栈顶元素，不出栈
+    public T peek(){
+
+        return myStack[top-1];
+    }
+}
