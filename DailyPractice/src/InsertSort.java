455cd2e3a92e7d90cf18a41b02eaba63f218dda6
eternityfantastic <1544152874@qq.com>
2019-11-27 22:28:51 +0800

    add

--- DailyPractice/src/InsertSort.java
+++ DailyPractice/src/InsertSort.java
@@ -2,12 +2,14 @@ import java.util.Arrays;
 
 public class InsertSort {
     public void insertSort(int[] arr) {
-        int len = arr.length;
+        int len ;
+        len = arr.length;
         for (int i = 1; i < len; i++) {
             int insert = arr[i];
             int j = i - 1;
-            for (; insert < arr[j] && j >= 0; j--) {
-                arr[j + 1] = arr[j];
+            while (insert < arr[j] && j >= 0) {
+                arr[j + 1] = arr[j--];
+                //j--;
             }
             arr[j + 1] = insert;
         }
@@ -15,17 +17,18 @@ public class InsertSort {
         System.out.println(Arrays.toString(arr));
     }
 
-    private int findIndex(int begin, int end, int k) {
+    private int findIndex(int[] arr, int begin, int end, int k) {
         int mid = 0;
         while (begin < end) {
             mid = (begin + end) / 2;
-            if (mid > k) {
+            if (arr[mid] > k) {
+                //if()
                 end = mid - 1;
-                findIndex(begin,end,k);
-            }else if(mid < k){
+                findIndex(arr, begin, end, k);
+            } else if (mid < k) {
                 begin = mid + 1;
-                findIndex(begin,end,k);
-            }else{
+                findIndex(arr, begin, end, k);
+            } else {
                 return mid;
             }
         }
@@ -38,5 +41,9 @@ class TestInsertSort {
     public static void main(String[] args) {
         InsertSort I = new InsertSort();
         I.insertSort(new int[]{1, 3, 2, 3, 6, 56, 6778, 3});
+        int a,b,c;
+
+
+
     }
 }
---
+++ JDBC_test01/src/DButil.java
@@ -0,0 +1,65 @@
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
+import java.sql.*;
+
+public class DButil {
+    public static final String DRIVER = "com.mysql.jdbc.Driver";//驱动路径
+    public static final String URL = "jdbc:mysql://localhost:3306/test";//连接数据库的路径
+    public static final String USER = "root";//帐号
+    public static final String PASSWORD = "root";//密码
+
+    /**
+     * 加载驱动器复制到lib
+     * 加载jdbc驱动并将其注册到DriverManager中  Class.forName(DRIVER);
+     * 建立数据库连接DriverManager.getConnection(URL, USER, PASSWORD);
+     * 建立statement（执行无参数的sql命令）或者preparedStatement（执行带参数的sql命令）对象；
+     * 执行sql
+     * 访问结果集resultSet
+     * 关闭对象
+     */
+
+    public static void main(String[] args) {
+        //此方法本质上就是为了生成conn对象，并返回！
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            MysqlDataSource mysqlDataSource = new MysqlDataSource();
+            ((MysqlDataSource)mysqlDataSource).setURL(URL);
+            mysqlDataSource.setUser(USER);
+            mysqlDataSource.setPassword(PASSWORD);
+            connection = mysqlDataSource.getConnection();
+
+//            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            String sql = "insert into stu"+"id,name,sex,birthday,amout,resume"+"value(?,?,?,?,?,?)";
+            statement = connection.prepareStatement(sql);
+            ((PreparedStatement) statement).setInt(1,1);
+            ((PreparedStatement) statement).setString(2,"小明");
+            ((PreparedStatement) statement).setByte(3,(byte) 3);
+//
+//            statement.execute("insert into test value(1,tom,null)");
+            resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                System.out.println();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (statement != null) {
+                    statement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
