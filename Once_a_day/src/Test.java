99a17554ea6675ab4b30d07cfff26337cc3ccc44
eternityfantastic <1544152874@qq.com>
2019-12-03 23:16:05 +0800

    add

--- Once_a_day/src/Test.java
+++ Once_a_day/src/Test.java
@@ -4,7 +4,7 @@ public class Test {
     public static void main(String[] args) {
         Day1 d = new Day1();
         int[] arr = new int[]{1,7,3,68,3,46,5,9,23};
-        d.shellSort(arr);
+        d.bubbleSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
