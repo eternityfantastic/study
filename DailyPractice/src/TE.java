89a7cc128b147bb081e747ff6d5a9ff92f53c5fa
eternityfantastic <1544152874@qq.com>
2019-12-03 23:15:27 +0800

    add

--- DailyPractice/src/TE.java
+++ DailyPractice/src/TE.java
@@ -1,12 +1,25 @@
-public class TE <T>{
-    public  void print(T[] array) {
+import java.util.ArrayList;
+import java.util.List;
+
+public class TE<T> {
+    private T t;
+    class TE2123 {
+        public <T> void print(T[] array) {
+            for (T a : array) {
+                System.out.println(a);
+            }
+        }
+    };
+
+
+    public void print(T[] array) {
         for (T a : array) {
             System.out.println(a);
         }
     }
 }
 
- class TE2 {
+class TE2 {
     public <T> void print(T[] array) {
         for (T a : array) {
             System.out.println(a);
@@ -15,16 +28,20 @@ public class TE <T>{
 }
 
 class TE3 {
-    public static  <T> void print(T[] array) {
+    //T只有实例化时才会确定，而static方法中要用泛型此时是泛型方法而不是泛型类
+    public static <T> void print(T[] array) {
         for (T a : array) {
             System.out.println(a);
         }
     }
 }
 
-class Testtt{
-    public static void main(String[] args) {
-        Integer[] arr = {1,2,3,4,5};
+class Testtt {
+    public static<T> void main(String[] args) {
+        Integer[] arr = {1, 2, 3, 4, 5};
         new TE().print(arr);
+        List<?> list =   new ArrayList<>();
+        List<T> list2 =   new ArrayList<>();
+
     }
 }
