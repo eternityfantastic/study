4c2e2a0677d7c326a50ac10a3445728ba33b23e8
eternityfantastic <1544152874@qq.com>
2019-12-14 00:04:36 +0800

    add

---
+++ Once_a_day/src/插入选择冒泡排序.java
@@ -0,0 +1,14 @@
+public interface 插入选择冒泡排序 {
+    void insertSort(int[] array);
+
+    void selectSort(int[] array);
+
+    void shellSort(int[] array);
+
+    void adjustDown(int[] elem, int root, int len);
+
+    void heapSort(int[] array);
+
+    void bubbleSort(int[] array);
+
+}
